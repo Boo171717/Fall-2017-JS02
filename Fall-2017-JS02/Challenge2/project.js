884e4a07eee97e86bac3142e87bf22e1611b6c05
Boo171717 <amitai.gillon@gmail.com>
2017-10-13 19:38:30 -0400

    THIS IS MY CHALLENGE 5 FINISHED!!!! :+1:
    YAY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Fall-2017-JS02/Challenge2/project.js
+++ Fall-2017-JS02/Challenge2/project.js
@@ -1,27 +1,104 @@
+let myInt = 7;
+let myString = "the number 7";
+let myNumString = "7";
+var first = myInt + myInt;
+var second = myInt + myString;
+var third = myInt + myNumString;
+var fourth = myNumString + myInt;
+var fifth = myInt + myNumString + myString;
+var sixth = myInt + myInt + myString;
+var seventh = myString + myInt + myInt;
+var eigth = myInt - myString;
+var ninth = myInt - myNumString;
+var tenth = myString - myNumString;
+var eleventh = myInt === myNumString;
+var twelfth = myInt === myNumString;
+/*<H1>Challenge 5: jQuery 2 - Variables and data Types</H1>
 
-      let myInt = 7;
-      let myString = "the number 7";
-      let myNumString = "7";
-      var first = myInt + myInt;
-      var second = myInt + myString;
-      var third = myInt + myNumString;
-      var fourth = myNumString + myInt;
-      var fifth = myInt + myNumString + myString;
-      var sixth = myInt + myInt + myString;
-      var seventh = myString + myInt + myInt;
-      var eigth = myInt - myString;
-      var ninth = myInt - myNumString;
-      var tenth = myString - myNumString;
-      var eleventh = myInt === myNumString;
-      var twelfth = myInt === myNumString;
-      /*<H1>Challenge 5: jQuery 2 - Variables and data Types</H1>
+<div id="challenge5vars">
+<H3>The values for this assignment</H3>
+</div>
 
-      <div id="challenge5vars">
-        <H3>The values for this assignment</H3>
-      </div>
-
-      <div id="challenge5predictions">
-        <H3>Checking my predictions</H3>
-      </div>*/
-    console.log("Hi there!! :)");
-    $("table").append(`<th><td>${first}</td></th><tr><th><td>${second}</td></th></tr><tr><th><td>${third}</td></th></tr><tr><th><td>${fourth}</td></th></tr><tr><th><td>${fifth}</td></th></tr><tr><th><td>${sixth}</td></th></tr><tr><th><td>${seventh}</td></th></tr><tr><th><td>${eigth}</td></th></tr><tr><th><td>${ninth}</td></th></tr><tr><th><td>${tenth}</td></th></tr><tr><th><td>${eleventh}</td></th></tr><tr><th><td>${twelfth}</td></th></tr>`)
+<div id="challenge5predictions">
+<H3>Checking my predictions</H3>
+</div>*/
+console.log("Hi there!! :)");
+$("table").append(`
+  <tr>
+    <th>myInt + mInt?</th>
+  <td>
+  ${first}
+  </td>
+  </tr>
+  <tr>
+    <th>myInt + myString?</th>
+  <td>
+  ${second}
+  </td>
+  </tr>
+  <tr>
+    <th>myInt + myNumString?</th>
+  <td>
+  ${third}
+  </td>
+  </tr>
+  <tr>
+    <th>myNumString + myInt?</th>
+  <td>
+  ${fourth}
+  </td>
+  </tr>
+  <tr>
+    <th>myInt + myNumString + myString?</th>
+  <td>
+  ${fifth}
+  </td>
+  </tr>
+  <tr>
+    <th>myInt + myInt + myString?</th>
+  <td>
+  ${sixth}
+  </td>
+  </tr>
+  <tr>
+    <th>myString + myInt + my Int?</th>
+  <td>
+  ${seventh}
+  </td>
+  </tr>
+  <tr>
+    <th>Subtraction</th>
+  </tr>
+  <tr>
+    <th>myInt - myString?</th>
+  <td>
+  ${eigth}
+  </td>
+  </tr>
+  <tr>
+    <th>myInt - myNumString?</th>
+  <td>
+  ${ninth}
+  </td>
+  </tr>
+  <tr>
+    <th>myString - myNumString?</th>
+  <td>
+  ${tenth}
+  </td>
+  </tr>
+  <tr>
+    <th>Comparisons</th>
+  </tr>
+  <tr>
+    <th>myInt == myNumString?</th>
+  <td>
+  ${eleventh}
+  </td>
+  </tr>
+  <tr>
+    <th>myInt === myNumString?</th>
+  <td>
+  ${twelfth}
+  </td>
+  </tr>`)
